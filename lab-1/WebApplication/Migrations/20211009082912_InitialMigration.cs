3b950426ff7da10e3be9d1224344f864a0094332
greatstudentkaze <dfyz0r@list.ru>
2021-10-09 11:35:10 +0300

    lab-1: run database migration

---
+++ lab-1/WebApplication/Migrations/20211009082912_InitialMigration.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentsGroups",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Number = table.Column<string>(type: "text", nullable: true),
+                    Course = table.Column<int>(type: "integer", nullable: false),
+                    StudentsCount = table.Column<int>(type: "integer", nullable: false),
+                    Department = table.Column<string>(type: "text", nullable: true),
+                    GraduatingDepartment = table.Column<string>(type: "text", nullable: true),
+                    GroupLeaderFullName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentsGroups", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentsGroups");
+        }
+    }
+}
