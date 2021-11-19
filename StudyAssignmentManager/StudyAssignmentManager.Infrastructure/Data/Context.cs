3d0e24e2859d3472025d7827e4682d3e24666e49
greatstudentkaze <dfyz0r@list.ru>
2021-11-19 17:11:50 +0300

    #SAM: Add Context class

---
+++ StudyAssignmentManager/StudyAssignmentManager.Infrastructure/Data/Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using StudyAssignmentManager.Domain;
+
+namespace StudyAssignmentManager.Infrastructure
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {}
+        
+        public DbSet<StudyAssignment> StudyAssignments { get; set; }
+        public DbSet<AssignmentData> AssignmentDatas { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+        public DbSet<CheckRequest> CheckRequests { get; set; }
+    }
+}
