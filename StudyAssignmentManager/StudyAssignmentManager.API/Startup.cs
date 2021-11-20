24b33dcd5313eb90341c373d38605a798834edb3
greatstudentkaze <dfyz0r@list.ru>
2021-11-20 03:27:17 +0300

    #SAM: Add DbContext to the services and add connection string

--- StudyAssignmentManager/StudyAssignmentManager.API/Startup.cs
+++ StudyAssignmentManager/StudyAssignmentManager.API/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,8 @@ namespace StudyAssignmentManager.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddDbContext<Infrastructure.Context>(options =>
+                options.UseNpgsql(Configuration.GetConnectionString("StudyAssignmentManager")));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "StudyAssignmentManager.API", Version = "v1"});
