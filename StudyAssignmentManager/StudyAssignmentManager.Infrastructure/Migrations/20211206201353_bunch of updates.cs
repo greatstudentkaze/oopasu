228e2828158aa902ff5b5ee1d31d8b32a881f4bc
greatstudentkaze <dfyz0r@list.ru>
2021-12-06 23:23:18 +0300

    #SAM: Update db

---
+++ StudyAssignmentManager/StudyAssignmentManager.Infrastructure/Migrations/20211206201353_bunch of updates.cs
@@ -0,0 +1,294 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using StudyAssignmentManager.Domain;
+
+namespace StudyAssignmentManager.Infrastructure.Migrations
+{
+    public partial class bunchofupdates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudyAssignments_AssignmentDatas_DataId",
+                table: "StudyAssignments");
+
+            migrationBuilder.DropTable(
+                name: "AssignmentDatas");
+
+            migrationBuilder.DropTable(
+                name: "UserUserRole");
+
+            migrationBuilder.DropTable(
+                name: "UserRoles");
+
+            migrationBuilder.DropColumn(
+                name: "RoleIds",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "AttachmentUrls",
+                table: "Answers");
+
+            migrationBuilder.DropColumn(
+                name: "Comments",
+                table: "Answers");
+
+            migrationBuilder.RenameColumn(
+                name: "DataId",
+                table: "StudyAssignments",
+                newName: "EducationalMaterialId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudyAssignments_DataId",
+                table: "StudyAssignments",
+                newName: "IX_StudyAssignments_EducationalMaterialId");
+
+            migrationBuilder.RenameColumn(
+                name: "Data",
+                table: "Answers",
+                newName: "Content");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Attachments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Url = table.Column<string>(type: "text", nullable: true),
+                    AnswerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Attachments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Attachments_Answers_AnswerId",
+                        column: x => x.AnswerId,
+                        principalTable: "Answers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Attachments_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Text = table.Column<string>(type: "text", nullable: true),
+                    AnswerId = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Answers_AnswerId",
+                        column: x => x.AnswerId,
+                        principalTable: "Answers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Comments_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EducationalMaterials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<EditorJSData>(type: "jsonb", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EducationalMaterials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EducationalMaterials_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachments_AnswerId",
+                table: "Attachments",
+                column: "AnswerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attachments_AuthorId",
+                table: "Attachments",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AnswerId",
+                table: "Comments",
+                column: "AnswerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_AuthorId",
+                table: "Comments",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EducationalMaterials_AuthorId",
+                table: "EducationalMaterials",
+                column: "AuthorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudyAssignments_EducationalMaterials_EducationalMaterialId",
+                table: "StudyAssignments",
+                column: "EducationalMaterialId",
+                principalTable: "EducationalMaterials",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_StudyAssignments_EducationalMaterials_EducationalMaterialId",
+                table: "StudyAssignments");
+
+            migrationBuilder.DropTable(
+                name: "Attachments");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "EducationalMaterials");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "EducationalMaterialId",
+                table: "StudyAssignments",
+                newName: "DataId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StudyAssignments_EducationalMaterialId",
+                table: "StudyAssignments",
+                newName: "IX_StudyAssignments_DataId");
+
+            migrationBuilder.RenameColumn(
+                name: "Content",
+                table: "Answers",
+                newName: "Data");
+
+            migrationBuilder.AddColumn<List<Guid>>(
+                name: "RoleIds",
+                table: "Users",
+                type: "uuid[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "AttachmentUrls",
+                table: "Answers",
+                type: "text[]",
+                nullable: true);
+
+            migrationBuilder.AddColumn<List<string>>(
+                name: "Comments",
+                table: "Answers",
+                type: "text[]",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "AssignmentDatas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    AuthorId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Data = table.Column<EditorJSData>(type: "jsonb", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Title = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AssignmentDatas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AssignmentDatas_Users_AuthorId",
+                        column: x => x.AuthorId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserRoles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserUserRole",
+                columns: table => new
+                {
+                    RolesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserUserRole", x => new { x.RolesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_UserUserRole_UserRoles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "UserRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserUserRole_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AssignmentDatas_AuthorId",
+                table: "AssignmentDatas",
+                column: "AuthorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserUserRole_UsersId",
+                table: "UserUserRole",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_StudyAssignments_AssignmentDatas_DataId",
+                table: "StudyAssignments",
+                column: "DataId",
+                principalTable: "AssignmentDatas",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
